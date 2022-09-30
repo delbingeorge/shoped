be2711e3cbac365d01e145e66b06d1914ce12eed
delbin <imdelbingeorge@gmail.com>
2022-09-30 13:32:14 +0530

    favicon added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import NavBar from "./pages/NavBar/NavBar";
 import HeroSection from "./pages/HeroSection/HeroSection";
 import Category from "./pages/Category/Category";
 import Footer from "./pages/Footer/Footer";
+import Favicon from "react-favicon";
+import Faviconfile from "../src/media/favicon.png";
 function App() {
     return (
         <div className="App" id="mainApp">
+            <Favicon url={Faviconfile} />
             <NavBar />
             <HeroSection />
             <Category />
