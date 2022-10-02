9d667b9a8a9141c345086a7d94fb7cd07f68e3ec
delbin <imdelbingeorge@gmail.com>
2022-10-02 15:15:18 +0530

    Added beauty products to the page and to the json thing!

--- src/pages/Product/Beauty/Beauty.js
+++ src/pages/Product/Beauty/Beauty.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faArrowDown, faIndianRupee } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp, faArrowDown, faIndianRupee, faCartPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
-import cosmicMerch from "./images/cosmicMerch.jpg";
-import tshirtMerch from "./images/tshirtMerch.png";
-import hoodyImg from "./images/hoodyImg.png";
-import redHoody from "./images/redHoody.png";
+import itemone from "./images/itemone.png";
+import itemtwo from "./images/itemtwo.png";
+import itemthree from "./images/itemthree.png";
+import itemfour from "./images/itemfour.png";
 function Beauty() {
     const [show, setShow] = useState(false);
     const [buttonText, setButtonText] = useState("View More");
     const [buttonIcon, setButtonIcon] = useState(faArrowDown);
+    const [value, setValue] = useState([]);
+    const fetchData = () => {
+        axios.get("https://delbingeorge.github.io/shopedapi/beauty.json").then((res) => {
+            setValue(res.data);
+        });
+    };
     function handleClick() {
         if (!show) {
             setButtonText("View Less");
@@ -26,73 +33,94 @@ function Beauty() {
                     <h1>Beauty & Cosmetics</h1>
                 </div>
                 <div className="product-div">
-                    <div className="product-item ">
-                        <img src={cosmicMerch} alt="Premium Merch by Cosmic"></img>
-                        <h1 className="product-title">Premium Merch by Cosmic</h1>
+                    <div className="product-item">
+                        <img src={itemone} alt="Nike Basketball Shoes"></img>
+                        <h1 className="product-title">Beauty Fairness Cream</h1>
                         <h1 className="product-price">
                             <FontAwesomeIcon icon={faIndianRupee} />
-                            7995.00
+                            500.00
                         </h1>
+                        <div className="item-action-btn">
+                            <button className="add-cart-btn">
+                                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                            </button>
+                            <button className="add-fav-btn">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </button>
+                        </div>
                     </div>
-                    <div className="product-item special-offer ">
-                        <img src={tshirtMerch} alt="Nike Casual Wear"></img>
-                        <h1 className="product-title">Nike Casual Wear</h1>
+                    <div className="product-item special-offer">
+                        <img src={itemtwo} alt="Beats Premium Sounds"></img>
+                        <h1 className="product-title">Head & Shoulder Hair Cream</h1>
                         <h1 className="product-price">
                             <FontAwesomeIcon icon={faIndianRupee} />
-                            995.00
+                            500.00
                         </h1>
+                        <div className="item-action-btn">
+                            <button className="add-cart-btn">
+                                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                            </button>
+                            <button className="add-fav-btn">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </button>
+                        </div>
                     </div>
                     <div className="product-item">
-                        <img src={hoodyImg} alt="Night Black Hoody"></img>
-                        <h1 className="product-title">Night Black Hoody</h1>
+                        <img src={itemthree} alt="Nintendo Switch Pro"></img>
+                        <h1 className="product-title">Axe Body Spray Men</h1>
                         <h1 className="product-price">
                             <FontAwesomeIcon icon={faIndianRupee} />
-                            795.00
+                            400.00
                         </h1>
+                        <div className="item-action-btn">
+                            <button className="add-cart-btn">
+                                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                            </button>
+                            <button className="add-fav-btn">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </button>
+                        </div>
                     </div>
                     <div className="product-item">
-                        <img src={redHoody} alt="Red Hoody Outfit"></img>
-                        <h1 className="product-title">Red Hoody Outfit</h1>
+                        <img src={itemfour} alt="Nike Basketball Shoes"></img>
+                        <h1 className="product-title">Nivia Refreshing Wash Gel</h1>
                         <h1 className="product-price">
                             <FontAwesomeIcon icon={faIndianRupee} />
-                            2995.00
+                            4590.00
                         </h1>
+                        <div className="item-action-btn">
+                            <button className="add-cart-btn">
+                                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                            </button>
+                            <button className="add-fav-btn">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </button>
+                        </div>
                     </div>
                 </div>
+
                 {show ? (
                     <div className="product-div">
-                        <div className="product-item">
-                            <img src={tshirtMerch} alt="Nike Casual Wear"></img>
-                            <h1 className="product-title">Nike Casual Wear</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={cosmicMerch} alt="Premium Merch by Cosmic"></img>
-                            <h1 className="product-title">Premium Merch by Cosmic</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                7995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={redHoody} alt="Red Hoody Outfit"></img>
-                            <h1 className="product-title">Red Hoody Outfit</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                2995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={hoodyImg} alt="Night Black Hoody"></img>
-                            <h1 className="product-title">Night Black Hoody</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                795.00
-                            </h1>
-                        </div>
+                        {value.map((val) => {
+                            return (
+                                <div className="product-item">
+                                    <img src={val.image} alt={val.title}></img>
+                                    <h1 className="product-title">{val.title}</h1>
+                                    <h1 className="product-price">
+                                        <FontAwesomeIcon icon={faIndianRupee} />
+                                        {val.price}
+                                    </h1>
+                                    <div className="item-action-btn">
+                                        <button className="add-cart-btn">
+                                            <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                                        </button>
+                                        <button className="add-fav-btn">
+                                            <FontAwesomeIcon icon={faHeart} />
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 ) : null}
 
@@ -102,6 +130,7 @@ function Beauty() {
                         onClick={() => {
                             setShow(!show);
                             handleClick();
+                            fetchData();
                         }}
                     >
                         <FontAwesomeIcon icon={buttonIcon} className="see-all-btn-icon" /> {buttonText}
