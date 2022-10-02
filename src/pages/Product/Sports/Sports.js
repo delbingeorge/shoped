1196dcffc2362dbb62419de9a7c8aa5a047641c1
delbin <imdelbingeorge@gmail.com>
2022-10-02 15:28:29 +0530

    Sports product added to the page

--- src/pages/Product/Sports/Sports.js
+++ src/pages/Product/Sports/Sports.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faIndianRupee, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faIndianRupee, faArrowUp, faArrowDown, faCartPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 import "../../Product/Product.css";
 import bootsImg from "../Sports/images/bootsImg.png";
@@ -12,6 +13,13 @@ function Sports() {
     const [show, setShow] = useState(false);
     const [buttonText, setButtonText] = useState("View More");
     const [buttonIcon, setButtonIcon] = useState(faArrowDown);
+    const [value, setValue] = useState([]);
+    const fetchData = () => {
+        axios.get("https://delbingeorge.github.io/shopedapi/sports.json").then((res) => {
+            setValue(res.data);
+        });
+    };
+
     function handleClick() {
         if (!show) {
             setButtonText("View Less");
@@ -62,40 +70,27 @@ function Sports() {
                     </div>
                 </div>
                 {show ? (
-                    <div className="product-div ">
-                        <div className="product-item">
-                            <img src={jerseyImg} alt="Men's Football Club Kit"></img>
-                            <h1 className="product-title">Men's Football Club Kit</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={shoeImg} alt="Nike Basketball Shoes"></img>
-                            <h1 className="product-title">Nike Basketball Shoes</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                2995.00
-                            </h1>
-                        </div>
-
-                        <div className="product-item ">
-                            <img src={bootsImg} alt="Mercurial Football Shoes"></img>
-                            <h1 className="product-title">Mercurial Football Shoes</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                7995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={footballImg} alt="Nike World Cup Football"></img>
-                            <h1 className="product-title">Nike World Cup Football</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                795.00
-                            </h1>
-                        </div>
+                    <div className="product-div">
+                        {value.map((val) => {
+                            return (
+                                <div className="product-item">
+                                    <img src={val.image} alt={val.title}></img>
+                                    <h1 className="product-title">{val.title}</h1>
+                                    <h1 className="product-price">
+                                        <FontAwesomeIcon icon={faIndianRupee} />
+                                        {val.price}
+                                    </h1>
+                                    <div className="item-action-btn">
+                                        <button className="add-cart-btn">
+                                            <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                                        </button>
+                                        <button className="add-fav-btn">
+                                            <FontAwesomeIcon icon={faHeart} />
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 ) : null}
                 <div className="btn-div">
@@ -104,6 +99,7 @@ function Sports() {
                         onClick={() => {
                             setShow(!show);
                             handleClick();
+                            fetchData();
                         }}
                     >
                         <FontAwesomeIcon icon={buttonIcon} className="see-all-btn-icon" /> {buttonText}
