40818e72dee9abd1d978eb7657e29546a2e8fd8e
delbin <imdelbingeorge@gmail.com>
2022-10-02 15:51:20 +0530

    add dress products

--- src/pages/Product/Fashion/Fashion.js
+++ src/pages/Product/Fashion/Fashion.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faArrowDown, faIndianRupee } from "@fortawesome/free-solid-svg-icons";
-
+import { faIndianRupee, faArrowDown, faArrowUp, faCartPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 import cosmicMerch from "./images/cosmicMerch.jpg";
 import tshirtMerch from "./images/tshirtMerch.png";
@@ -11,6 +11,13 @@ function Fashion() {
     const [show, setShow] = useState(false);
     const [buttonText, setButtonText] = useState("View More");
     const [buttonIcon, setButtonIcon] = useState(faArrowDown);
+    const [value, setValue] = useState([]);
+    const fetchData = () => {
+        axios.get("https://delbingeorge.github.io/shopedapi/fashion.json").then((res) => {
+            setValue(res.data);
+        });
+    };
+
     function handleClick() {
         if (!show) {
             setButtonText("View Less");
@@ -62,38 +69,26 @@ function Fashion() {
                 </div>
                 {show ? (
                     <div className="product-div">
-                        <div className="product-item">
-                            <img src={tshirtMerch} alt="Nike Casual Wear"></img>
-                            <h1 className="product-title">Nike Casual Wear</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={cosmicMerch} alt="Premium Merch by Cosmic"></img>
-                            <h1 className="product-title">Premium Merch by Cosmic</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                7995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={redHoody} alt="Red Hoody Outfit"></img>
-                            <h1 className="product-title">Red Hoody Outfit</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                2995.00
-                            </h1>
-                        </div>
-                        <div className="product-item">
-                            <img src={hoodyImg} alt="Night Black Hoody"></img>
-                            <h1 className="product-title">Night Black Hoody</h1>
-                            <h1 className="product-price">
-                                <FontAwesomeIcon icon={faIndianRupee} />
-                                795.00
-                            </h1>
-                        </div>
+                        {value.map((val) => {
+                            return (
+                                <div className="product-item">
+                                    <img src={val.image} alt={val.title}></img>
+                                    <h1 className="product-title">{val.title}</h1>
+                                    <h1 className="product-price">
+                                        <FontAwesomeIcon icon={faIndianRupee} />
+                                        {val.price}
+                                    </h1>
+                                    <div className="item-action-btn">
+                                        <button className="add-cart-btn">
+                                            <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+                                        </button>
+                                        <button className="add-fav-btn">
+                                            <FontAwesomeIcon icon={faHeart} />
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 ) : null}
 
@@ -103,6 +98,7 @@ function Fashion() {
                         onClick={() => {
                             setShow(!show);
                             handleClick();
+                            fetchData();
                         }}
                     >
                         <FontAwesomeIcon icon={buttonIcon} className="see-all-btn-icon" /> {buttonText}
